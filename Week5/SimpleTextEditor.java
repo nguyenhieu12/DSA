d9a23b31f01f12b0741b4e91ee539b23dff9c820
nguyenhieu12 <vailinhhon098@gmail.com>
2021-10-15 16:44:39 +0700

    update

--- Week5/SimpleTextEditor.java
+++ Week5/SimpleTextEditor.java
@@ -3,30 +3,54 @@ package Week5;
 import java.util.Scanner;
 
 public class SimpleTextEditor {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static String s = "";
+    private static Stack<String> stack = new Stack<String>();
+
+    public static void append() {
+        s += scanner.next();
+        stack.push(s);
+    }
+
+    public static void delete() {
+        if(scanner.nextInt() > s.length())
+            s = "";
+        else if(scanner.nextInt() < 0)
+            return;
+        else s = s.substring(0, s.length() - scanner.nextInt());
+        stack.push(s);
+    }
+
+    public static void print() {
+        if(scanner.nextInt() > s.length())
+            System.out.println("");
+        else System.out.println(s.charAt(scanner.nextInt() - 1));
+    }
+
+    public static void undo() {
+        stack.pop();
+        s = stack.peek();
+    }
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String s = "";
-        Stack<String> stack = new Stack<String>();
         stack.push(s);
         int n, choice;
         n = scanner.nextInt();
         for(int i = 0;i < n;i++) {
             choice = scanner.nextInt();
             if(choice == 1) {
-                s += scanner.next();
-                stack.push(s);
+                append();
             }
             else if(choice == 2) {
-                s = s.substring(0, s.length() - scanner.nextInt());
-                stack.push(s);
+                delete();
             }
             else if(choice == 3) {
-                System.out.println(s.charAt(scanner.nextInt() - 1));
+                print();
             }
             else {
-                stack.pop();
-                s = stack.peek();
+                undo();
             }
         }
     }
 }
+
